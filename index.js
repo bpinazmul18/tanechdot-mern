dc5b9ac01a901e58f1f8a1b20a7465e960df616f
asif-96 <shuyeb.dk@gmail.com>
2022-08-10 12:19:38 +0600

    add footer and some changes on index file

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app.use("/", profileRoutes);
 console.log(`${process.env.NODE_ENV}`);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve((__dirname + '/frontend/build'))));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname + '/frontend/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
 }
 
