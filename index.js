be2f1655aed21ff1b65c6b7be87325157cd6d650
asif-96 <shuyeb.dk@gmail.com>
2022-08-10 12:43:07 +0600

    solve some error

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("/", profileRoutes);
 console.log(`${process.env.NODE_ENV}`);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.use(express.static(path.join(__dirname, '/frontend/build/')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
